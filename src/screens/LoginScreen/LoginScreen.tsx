de17f52cc5b6c76b7b66abfd5898c881f3b4d9f1
gusmcardoso <gmcardoso.2003@gmail.com>
2023-06-27 19:38:43 -0300

    27/06

---
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useTheme } from "styled-components/native";
+import * as S from './LoginScreen.styles';
+export function LoginScreen() {
+    const {COLORS} = useTheme();
+    return <S.Container />;
+}
---
+++ src/theme/index.ts
@@ -0,0 +1,9 @@
+export default {
+    COLORS: {
+        WHITE: '#FFFFFF',
+        PRIMARY: '#5F2EE2',
+        GREEN: 'green',
+        NEUTRAL_DARK: '#ADADB8',
+        DANGER: '#F9695E'
+    }
+}
